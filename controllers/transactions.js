4a05dadf1db1b4858bf8c582ce77b1215ef0de00
esinghrahul <e.singh.rahul@outlook.com>
2020-07-08 15:20:16 +0530

    Added transaction model and connected to database

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,14 +1,28 @@
+const Transaction = require('../models/Transaction')
+
 //Get Transaction - GET
-exports.getTransactions = (req, res, next) => {
-    res.send('GET transactions')
+exports.getTransactions = async(req, res, next) => {
+    try{
+        const transactions = await Transaction.find()
+        return res.status(200).json({
+            success: true,
+            count: transactions.length,
+            data: transactions
+        })
+    }catch{
+        res.send(500).json({
+            success: false,
+            error: 'Server error'
+        })
+    }
 }
 
 //Add Transaction - POST
-exports.addTransaction = (req, res, next) => {
+exports.addTransaction = async(req, res, next) => {
     res.send('POST transaction')
 }
 
 //Delete Transaction
-exports.deleteTransaction = (req, res, next) => {
+exports.deleteTransaction = async(req, res, next) => {
     res.send('Delete transaction')
 }
--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express')
 const dotenv = require('dotenv')
 const colors = require('colors')
 const morgan = require('morgan')
-
+const connectDB = require('./config/db')
 const transactions = require('./routes/transactions')
 dotenv.config({path: './config/config.env'})
 
-
+connectDB()
 const app = express()
 
 app.use('/api/v1/transactions', transactions)
